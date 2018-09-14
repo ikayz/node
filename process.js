1f6ec178435409958a7254f7e4613cf0bfa8c96c
Isaac Ikayz <ikayz@live.com>
2018-09-14 14:30:56 +0200

    Using process to accept and echo back user input

--- process.js
+++ process.js
@@ -12,3 +12,35 @@ if (!user || !greeting) {
     // This will only work with node process --user YOURUSER --greeting "YOUR GREETING" in the terminal
     console.log(`Welcome ${user}, ${greeting}`);
 }
+
+// Accepting input
+var questions = [
+    "What is your name?",
+    "Where do you live?",
+    "What is your favourite programming language?"
+]
+
+var res = [];
+
+function ask(i) {
+    process.stdout.write(`\n\n\n ${questions[i]}`);
+    process.stdout.write(" > ");
+}
+
+process.stdin.on('data', function(data) {
+    res.push(data.toString().trim());
+
+    if(res.length < questions.length) {
+        ask(res.length);
+    } else {
+        process.exit();
+    }
+});
+
+process.on('exit', function(){
+    process.stdout.write('\n\n\n');
+    process.stdout.write(`You are ${res[0]}, you love ${res[2]} and live in ${res[1]}`);
+    process.stdout.write('\n\n\n');
+});
+
+ask(0);
